cbdecb6efa34b6b969b1ce0d341a671b8b2af327
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-04-22 21:01:46 +0300

    Added Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import './App.css'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   
 
   return (
     <>
-      
+      <NavBar/>
+      <Footer/>
     </>
   )
 }
