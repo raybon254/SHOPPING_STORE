c46d64900cee2f6de228e02b1b4e5d157b2d5d1e
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-04-25 01:00:44 +0300

    Uncommented  Routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,11 @@
 import Home from "./pages/Home";
-// import About from "./pages/About";
+import About from "./pages/About";
 // import Cart from "./pages/Cart";
 import ErrorPage from "./pages/ErrorPage"
 
 const routes = [
   { path: "/", element: <Home /> },
-  //   { path: "/about", element: <About /> },
+    { path: "/about", element: <About /> },
   //   { path: "/cart", element: <Cart /> },
   { path: "*", element: <ErrorPage /> },
 ];
