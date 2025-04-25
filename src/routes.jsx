e7a6f6cf2835f5c1ded033946cadbb22a17bd3b0
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-04-25 12:27:31 +0300

    Formatted Code

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,12 @@
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Cart from "./pages/Cart";
-import ErrorPage from "./pages/ErrorPage"
+import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
   { path: "/", element: <Home /> },
-    { path: "/about", element: <About /> },
-    { path: "/cart", element: <Cart /> },
+  { path: "/about", element: <About /> },
+  { path: "/cart", element: <Cart /> },
   { path: "*", element: <ErrorPage /> },
 ];
 
