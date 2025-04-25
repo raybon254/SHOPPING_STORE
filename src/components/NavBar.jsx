4797496aee5000f3e901839251b3891b1d8bc1e4
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-04-25 20:13:00 +0300

    Added 2 new pages and added an isActive to NavBar Links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import cartIcon from "../assets/cart.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
+  const location = useLocation();
+
+  const isActive = (path) =>
+    location.pathname === path ? "text-success fw-bold" : "";
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="text-dark">
       <Container>
         <Navbar.Brand href="#home">
           <img
@@ -17,29 +22,32 @@ const NavBar = () => {
           />
         </Navbar.Brand>
 
-        <Navbar.Brand href="#home" className="fw-bold font-monospace fs-4">Shopping Cart</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="fw-medium  fs-4">
+          Shopping  <span className="fw-medium fs-4  text-success mx-1"> Store</span>
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto mx-4 p-2  fs-5 ">
-            <Nav.Link as={Link} to="/">
+          <Nav className="ms-auto mx-4 p-2 fs-5">
+            <Nav.Link as={Link} to="/" className={isActive("/")}>
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/products">
+            <Nav.Link as={Link} to="/products" className={isActive("/products")}>
               Products
             </Nav.Link>
-            <Nav.Link as={Link} to="/about">
+            <Nav.Link as={Link} to="/about" className={isActive("/about")}>
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="/cart">
+            <Nav.Link as={Link} to="/cart" className={isActive("/cart")}>
               Cart
             </Nav.Link>
 
             <NavDropdown title="Log in" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
+              <NavDropdown.Item as={Link} to="/login-user" className={isActive("/login-user")}>
                 Log in As User
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Log in As Admin{" "}
+              <NavDropdown.Item as={Link} to="/login-admin" className={isActive("/login-admin")}>
+                Log in As Admin
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
