a3c527f0585ea311a16829deae717cbd6208d592
Brian <musyimib449@gmail.com>
2025-04-25 12:01:06 +0300

    global fetch fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PostData } from './components/ContextPost.jsx';
+import { Fetch, useFetch } from './components/ContextFetch.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App.jsx'
@@ -8,7 +9,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <PostData>
-           <App />
+           <Fetch>
+                <App /> 
+           </Fetch>
       </PostData>
   </StrictMode>,
 )
