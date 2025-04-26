c8d5928fe24e735aaadc8009ae394a90008dcc17
Brian <musyimib449@gmail.com>
2025-04-26 12:12:28 +0300

    styling

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -38,7 +38,7 @@ const Products = () => {
 
   return (
     <div className="container py-5 ">
-        <h1 className="mb-4 text-center fw-medium text-secondary">Our Products</h1>
+        <h1 className="mb-4 text-center fw-medium ">Our Products</h1>
 
     {/* diplay row per category from fetch */}
         {
